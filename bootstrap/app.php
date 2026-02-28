e1fec2fb9d2f5cbba4ef4829a99254c40f133d7f
Storm-ranger <antonetterevillosa@gmail.com>
2026-02-28 23:36:04 +0800

    fixed bootstrap app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,9 +28,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     
-    ->withMiddleware(function (Middleware $middleware) {
-    $middleware->append(\App\Http\Middleware\TrustProxies::class);
-})
+
+    //->withMiddleware(function (Middleware $middleware) {
+    //$middleware->append(\App\Http\Middleware\TrustProxies::class);
+//})
 
 
     ->withExceptions(function (Exceptions $exceptions): void {
