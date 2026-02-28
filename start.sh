5867b3c1fe228d77c7f092d6b451ea6944e0c5be
Storm-ranger <antonetterevillosa@gmail.com>
2026-02-28 19:07:25 +0800

    Fix Railway port

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-set -e
 
-php artisan config:clear || true
-php artisan cache:clear || true
-php artisan route:clear || true
-php artisan view:clear || true
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
 
 php artisan migrate --force
 
